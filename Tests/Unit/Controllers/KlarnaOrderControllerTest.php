151d542363cf9a7a1b2b4ccb930b7eae37182efb
Arkadiusz Kostrzeba <a.kostrzeba@creatit.pl>
2018-04-19 11:17:42 +0200

    Tests

--- Tests/Unit/Controllers/KlarnaOrderControllerTest.php
+++ Tests/Unit/Controllers/KlarnaOrderControllerTest.php
@@ -8,9 +8,16 @@
 
 namespace TopConcepts\Klarna\Tests\Unit\Controllers;
 
-use TopConcepts\Klarna\Controllers\KlarnaOrderController;
 
-class KlarnaOrderControllerTest extends \PHPUnit_Framework_TestCase
+use OxidEsales\Eshop\Application\Controller\OrderController;
+use OxidEsales\Eshop\Application\Model\Basket;
+use OxidEsales\Eshop\Core\Controller\BaseController;
+use OxidEsales\Eshop\Core\Registry;
+use OxidEsales\PayPalModule\Controller\ExpressCheckoutDispatcher;
+use TopConcepts\Klarna\Controllers\KlarnaOrderController;
+use TopConcepts\Klarna\Tests\Unit\ModuleUnitTestCase;
+
+class KlarnaOrderControllerTest extends ModuleUnitTestCase
 {
 
     public function testIsPayPalAmazon()
@@ -53,13 +60,47 @@ class KlarnaOrderControllerTest extends \PHPUnit_Framework_TestCase
 
     }
 
-    public function testKlarnaExternalCheckout()
+    public function klarnaExternalCheckoutDP()
     {
+        return [
+            ['bestitamazon', 0, $this->getConfig()->getSslShopUrl() . 'index.php?cl=KlarnaEpmDispatcher&fnc=amazonLogin'],
+            ['oxidpaypal', 1, null],
+            ['other', 0, $this->getConfig()->getSslShopUrl() . 'index.php?cl=KlarnaExpress']
+        ];
+    }
 
+    /**
+     * @dataProvider klarnaExternalCheckoutDP
+     * @param $paymentId
+     * @param $dispatcherCallsCount
+     * @param $rUrl
+     */
+    public function testKlarnaExternalCheckout($paymentId, $dispatcherCallsCount, $rUrl)
+    {
+        $dispatcher = $this->getMock(BaseController::class, ['setExpressCheckout']);
+        $dispatcher->expects($this->exactly($dispatcherCallsCount))->method('setExpressCheckout');
+        \oxTestModules::addModuleObject(ExpressCheckoutDispatcher::class, $dispatcher);
+        $oOrderController = oxNew(OrderController::class);
+        $oOrderController->klarnaExternalCheckout($paymentId);
+        $this->assertEquals($rUrl, \oxUtilsHelper::$sRedirectUrl);
     }
 
     public function testIncludeKPWidget()
     {
+        $oSession = Registry::getSession();
+        $oBasket = oxNew(Basket::class);
+        $oBasket->setPayment('other');
+        $oSession->setBasket($oBasket);
+        $oSession->freeze();
+        $oOrderController = oxNew(OrderController::class);
+        $this->assertFalse($oOrderController->includeKPWidget());
 
+        $oSession = Registry::getSession();
+        $oBasket = oxNew(Basket::class);
+        $oBasket->setPayment('klarna_pay_now');
+        $oSession->setBasket($oBasket);
+        $oSession->freeze();
+        $oOrderController = oxNew(OrderController::class);
+        $this->assertTrue($oOrderController->includeKPWidget());
     }
 }
