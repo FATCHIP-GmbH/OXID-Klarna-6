87259fcd55c3d264c7b2514b2fd46907afea7176
Grzegorz Dabrowski <g.dabrowski@createit.pl>
2018-04-20 15:17:52 +0200

    fix KlarnaInstallerTest

--- Tests/Unit/Core/KlarnaInstallerTest.php
+++ Tests/Unit/Core/KlarnaInstallerTest.php
@@ -42,32 +42,25 @@ class KlarnaInstallerTest extends ModuleUnitTestCase
             "ALTER TABLE oxorder DROP `KLORDERID`",
             "ALTER TABLE oxorder DROP `KLSYNC`",
 
-//            "ALTER TABLE oxorderarticles DROP `KLTITLE`",
-//            "ALTER TABLE oxorderarticles DROP `KLARTNUM`",
-//
-//            "ALTER TABLE oxpayments DROP `KLPAYMENTTYPES`",
-//            "ALTER TABLE oxpayments DROP `KLEXTERNALNAME`",
-//            "ALTER TABLE oxpayments DROP `KLEXTERNALPAYMENT`",
-//            "ALTER TABLE oxpayments DROP `KLEXTERNALCHECKOUT`",
-//            "ALTER TABLE oxpayments DROP `KLPAYMENTIMAGEURL`",
-//            "ALTER TABLE oxpayments DROP `KLPAYMENTIMAGEURL_1`",
-//            "ALTER TABLE oxpayments DROP `KLCHECKOUTIMAGEURL`",
-//            "ALTER TABLE oxpayments DROP `KLCHECKOUTIMAGEURL_1`",
-//            "ALTER TABLE oxpayments DROP `KLPAYMENTOPTION`",
-//            "ALTER TABLE oxpayments DROP `KLEMDPURCHASEHISTORYFULL`",
-//
-//            "ALTER TABLE oxaddress DROP `KLTEMPORARY`",
-//
-//            "ALTER TABLE kl_logs DROP `KLORDERID`",
-//            "ALTER TABLE kl_logs DROP `KLMID`",
-//            "ALTER TABLE kl_logs DROP `KLSTATUSCODE`",
-//            "ALTER TABLE kl_logs DROP `KLURL`",
-//
-//            "ALTER TABLE kl_ack DROP `KLORDERID`",
-//
-//            "DROP TABLE IF EXISTS `kl_ack`",
-//            "DROP TABLE IF EXISTS `kl_logs`",
-//            "DROP TABLE IF EXISTS `kl_anon_lookup`",
+            "ALTER TABLE oxorderarticles DROP `KLTITLE`",
+            "ALTER TABLE oxorderarticles DROP `KLARTNUM`",
+
+            "ALTER TABLE oxpayments DROP `KLPAYMENTTYPES`",
+            "ALTER TABLE oxpayments DROP `KLEXTERNALNAME`",
+            "ALTER TABLE oxpayments DROP `KLEXTERNALPAYMENT`",
+            "ALTER TABLE oxpayments DROP `KLEXTERNALCHECKOUT`",
+            "ALTER TABLE oxpayments DROP `KLPAYMENTIMAGEURL`",
+            "ALTER TABLE oxpayments DROP `KLPAYMENTIMAGEURL_1`",
+            "ALTER TABLE oxpayments DROP `KLCHECKOUTIMAGEURL`",
+            "ALTER TABLE oxpayments DROP `KLCHECKOUTIMAGEURL_1`",
+            "ALTER TABLE oxpayments DROP `KLPAYMENTOPTION`",
+            "ALTER TABLE oxpayments DROP `KLEMDPURCHASEHISTORYFULL`",
+
+            "ALTER TABLE oxaddress DROP `KLTEMPORARY`",
+
+            "DROP TABLE IF EXISTS `kl_ack`",
+            "DROP TABLE IF EXISTS `kl_logs`",
+            "DROP TABLE IF EXISTS `kl_anon_lookup`",
         ]);
     }
 
@@ -129,6 +122,7 @@ class KlarnaInstallerTest extends ModuleUnitTestCase
     {
         $this->setProtectedClassProperty(KlarnaInstaller::getInstance(), 'instance', null);
         $result     = KlarnaInstaller::getInstance();
+
         $dbName     = $this->getProtectedClassProperty($result, 'dbName');
         $modulePath = $this->getProtectedClassProperty($result, 'modulePath');
         $db         = $this->getProtectedClassProperty($result, 'db');
@@ -142,6 +136,7 @@ class KlarnaInstallerTest extends ModuleUnitTestCase
     public function testOnActivate()
     {
         $this->revertTestSuiteSetup();
+
         $dbMetaDataHandler = oxNew(DbMetaDataHandler::class);
         $db                = DatabaseProvider::getDb(DatabaseProvider::FETCH_MODE_ASSOC);
         $db->execute('DELETE FROM oxmigrations_ce WHERE version = ?', ['Klarna400']);
@@ -168,30 +163,29 @@ class KlarnaInstallerTest extends ModuleUnitTestCase
         $this->assertTrue($dbMetaDataHandler->fieldExists('KLSERVERMODE', 'oxorder'));
         $this->assertTrue($dbMetaDataHandler->fieldExists('KLORDERID', 'oxorder'));
         $this->assertTrue($dbMetaDataHandler->fieldExists('KLSYNC', 'oxorder'));
-//
-//        $this->assertTrue($dbMetaDataHandler->fieldExists('KLTITLE', 'oxorderarticles'));
-//        $this->assertTrue($dbMetaDataHandler->fieldExists('KLARTNUM', 'oxorderarticles'));
-//
-//        $this->assertTrue($dbMetaDataHandler->fieldExists('KLPAYMENTTYPES', 'oxpayments'));
-//        $this->assertTrue($dbMetaDataHandler->fieldExists('KLEXTERNALNAME', 'oxpayments'));
-//        $this->assertTrue($dbMetaDataHandler->fieldExists('KLEXTERNALPAYMENT', 'oxpayments'));
-//        $this->assertTrue($dbMetaDataHandler->fieldExists('KLEXTERNALCHECKOUT', 'oxpayments'));
-//        $this->assertTrue($dbMetaDataHandler->fieldExists('KLPAYMENTIMAGEURL', 'oxpayments'));
-//        $this->assertTrue($dbMetaDataHandler->fieldExists('KLPAYMENTIMAGEURL_1', 'oxpayments'));
-//        $this->assertTrue($dbMetaDataHandler->fieldExists('KLCHECKOUTIMAGEURL', 'oxpayments'));
-//        $this->assertTrue($dbMetaDataHandler->fieldExists('KLCHECKOUTIMAGEURL_1', 'oxpayments'));
-//        $this->assertTrue($dbMetaDataHandler->fieldExists('KLPAYMENTOPTION', 'oxpayments'));
-//        $this->assertTrue($dbMetaDataHandler->fieldExists('KLEMDPURCHASEHISTORYFULL', 'oxpayments'));
-//
-//        $this->assertTrue($dbMetaDataHandler->fieldExists('KLTEMPORARY', 'oxaddress'));
-//
-//        $this->assertTrue($dbMetaDataHandler->fieldExists('KLORDERID', 'kl_logs'));
-//        $this->assertTrue($dbMetaDataHandler->fieldExists('KLMID', 'kl_logs'));
-//        $this->assertTrue($dbMetaDataHandler->fieldExists('KLSTATUSCODE', 'kl_logs'));
-//        $this->assertTrue($dbMetaDataHandler->fieldExists('KLURL', 'kl_logs'));
-//
-//        $this->assertTrue($dbMetaDataHandler->fieldExists('KLORDERID', 'kl_ack'));
-//        $dbMetaDataHandler->updateViews();
+
+        $this->assertTrue($dbMetaDataHandler->fieldExists('KLTITLE', 'oxorderarticles'));
+        $this->assertTrue($dbMetaDataHandler->fieldExists('KLARTNUM', 'oxorderarticles'));
+
+        $this->assertTrue($dbMetaDataHandler->fieldExists('KLPAYMENTTYPES', 'oxpayments'));
+        $this->assertTrue($dbMetaDataHandler->fieldExists('KLEXTERNALNAME', 'oxpayments'));
+        $this->assertTrue($dbMetaDataHandler->fieldExists('KLEXTERNALPAYMENT', 'oxpayments'));
+        $this->assertTrue($dbMetaDataHandler->fieldExists('KLEXTERNALCHECKOUT', 'oxpayments'));
+        $this->assertTrue($dbMetaDataHandler->fieldExists('KLPAYMENTIMAGEURL', 'oxpayments'));
+        $this->assertTrue($dbMetaDataHandler->fieldExists('KLPAYMENTIMAGEURL_1', 'oxpayments'));
+        $this->assertTrue($dbMetaDataHandler->fieldExists('KLCHECKOUTIMAGEURL', 'oxpayments'));
+        $this->assertTrue($dbMetaDataHandler->fieldExists('KLCHECKOUTIMAGEURL_1', 'oxpayments'));
+        $this->assertTrue($dbMetaDataHandler->fieldExists('KLPAYMENTOPTION', 'oxpayments'));
+        $this->assertTrue($dbMetaDataHandler->fieldExists('KLEMDPURCHASEHISTORYFULL', 'oxpayments'));
+
+        $this->assertTrue($dbMetaDataHandler->fieldExists('KLTEMPORARY', 'oxaddress'));
+
+        $this->assertTrue($dbMetaDataHandler->fieldExists('KLORDERID', 'kl_logs'));
+        $this->assertTrue($dbMetaDataHandler->fieldExists('KLMID', 'kl_logs'));
+        $this->assertTrue($dbMetaDataHandler->fieldExists('KLSTATUSCODE', 'kl_logs'));
+        $this->assertTrue($dbMetaDataHandler->fieldExists('KLURL', 'kl_logs'));
+
+        $this->assertTrue($dbMetaDataHandler->fieldExists('KLORDERID', 'kl_ack'));
     }
 
     public function testOnActivateConfigVars()
