3ec3d62839ead0cd3f260a8d23dab3d39755e13e
Arkadiusz Kostrzeba <a.kostrzeba@creatit.pl>
2018-07-14 12:28:20 +0200

    article2shop for EE

--- Tests/additional.inc.php
+++ Tests/additional.inc.php
@@ -1,4 +1,5 @@
 <?php
+use \OxidEsales\Eshop\Core\DatabaseProvider;
 
 $serviceCaller = new \OxidEsales\TestingLibrary\ServiceCaller();
 $testConfig = new \OxidEsales\TestingLibrary\TestConfig();
@@ -7,6 +8,17 @@ $testDirectory = $testConfig->getEditionTestsPath($testConfig->getShopEdition())
 $klarnaTestDirectory = $testConfig->getShopPath() . 'modules//tc/tcklarna/Tests/';
 $serviceCaller->setParameter('importSql', '@' . $testDirectory . '/Fixtures/testdata.sql');
 $serviceCaller->setParameter('importSql', '@' . $klarnaTestDirectory . 'Unit/Testdata/klarna-settings.sql');
+
+
+/** Add object to shop mapping for EE */
+if ($testConfig->getShopEdition() == 'EE'){
+    $db = DatabaseProvider::getDb();
+    $sql = "REPLACE INTO `oxarticles2shop` SET `oxmapobjectid` = ?, `oxshopid` = ?";
+    $db->execute($sql, array(1, 1));
+    $db->execute($sql, array(2, 1));
+    $db->execute($sql, array(3, 1));
+}
+
 $serviceCaller->callService('ShopPreparation', 1);
 
 define('oxADMIN_LOGIN', oxDb::getDb()->getOne("select OXUSERNAME from oxuser where oxid='oxdefaultadmin'"));
