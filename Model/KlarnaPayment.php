8ffc3ee86a6d4d669e960746abfbcf582e416beb
Hugo Ferreira <h.ferreira@createit.pl>
2019-09-03 11:35:09 +0200

    Version 5 - Pay split

--- Model/KlarnaPayment.php
+++ Model/KlarnaPayment.php
@@ -62,6 +62,20 @@ class KlarnaPayment extends KlarnaPayment_parent
     const KLARNA_PAYMENT_PAY_NOW = 'klarna_pay_now';
 
     /**
+     * Oxid value of Klarna Pay Now payment
+     *
+     * @var string
+     */
+    const KLARNA_DIRECTDEBIT = 'klarna_directdebit';
+
+    /**
+     * Oxid value of Klarna Pay Now payment
+     *
+     * @var string
+     */
+    const KLARNA_SOFORT = 'klarna_sofort';
+
+    /**
      * Get list of Klarna payments ids
      *
      * @param null||string $filter KP - Klarna Payment Options
@@ -75,7 +89,8 @@ class KlarnaPayment extends KlarnaPayment_parent
                 self::KLARNA_PAYMENT_SLICE_IT_ID,
                 self::KLARNA_PAYMENT_PAY_LATER_ID,
                 self::KLARNA_PAYMENT_PAY_NOW,
-
+                self::KLARNA_DIRECTDEBIT,
+                self::KLARNA_SOFORT,
             );
         }
         if ($filter === 'KP') {
@@ -83,6 +98,8 @@ class KlarnaPayment extends KlarnaPayment_parent
                 self::KLARNA_PAYMENT_SLICE_IT_ID,
                 self::KLARNA_PAYMENT_PAY_LATER_ID,
                 self::KLARNA_PAYMENT_PAY_NOW,
+                self::KLARNA_DIRECTDEBIT,
+                self::KLARNA_SOFORT,
             );
         }
     }
@@ -98,6 +115,8 @@ class KlarnaPayment extends KlarnaPayment_parent
                 self::KLARNA_PAYMENT_SLICE_IT_ID  => 'pay_over_time',
                 self::KLARNA_PAYMENT_PAY_LATER_ID => 'pay_later',
                 self::KLARNA_PAYMENT_PAY_NOW      => 'pay_now',
+                self::KLARNA_DIRECTDEBIT          => 'direct_debit',
+                self::KLARNA_SOFORT               => 'direct_bank_transfer',
             );
 
             return $names[$this->getId()];
