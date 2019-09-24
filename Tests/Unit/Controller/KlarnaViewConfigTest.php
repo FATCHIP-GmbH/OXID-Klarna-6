16ff5f558a4a3b0603aee84576ba7737a288fa91
Arkadiusz Kostrzeba <kostrzeba@cgrd.de>
2019-09-24 10:48:37 +0200

    Fix: saveShopConfVar with type null

--- Tests/Unit/Controller/KlarnaViewConfigTest.php
+++ Tests/Unit/Controller/KlarnaViewConfigTest.php
@@ -20,7 +20,7 @@ class KlarnaViewConfigTest extends ModuleUnitTestCase
 
     public function testAddBuyNow()
     {
-        $this->getConfig()->saveShopConfVar(null, 'blKlarnaDisplayBuyNow', true, null, 'module:tcklarna');
+        $this->getConfig()->saveShopConfVar('bool', 'blKlarnaDisplayBuyNow', true, null, 'module:tcklarna');
 
         $oViewConfig = oxNew(ViewConfig::class);
         $result = $oViewConfig->addBuyNow();
@@ -136,8 +136,8 @@ class KlarnaViewConfigTest extends ModuleUnitTestCase
     public function testGetKlarnaHomepageBanner($displayBanner, $merchantId)
     {
 
-        $this->getConfig()->saveShopConfVar(null, 'blKlarnaDisplayBanner', $displayBanner, $this->getShopId(), 'module:tcklarna');
-        $this->getConfig()->saveShopConfVar(null, 'sKlarnaMerchantId', $merchantId, $this->getShopId(), 'module:tcklarna');
+        $this->getConfig()->saveShopConfVar('bool', 'blKlarnaDisplayBanner', $displayBanner, $this->getShopId(), 'module:tcklarna');
+        $this->getConfig()->saveShopConfVar('str', 'sKlarnaMerchantId', $merchantId, $this->getShopId(), 'module:tcklarna');
 
         $oViewConfig = oxNew(ViewConfig::class);
         $result = $oViewConfig->getKlarnaHomepageBanner();
@@ -198,7 +198,7 @@ class KlarnaViewConfigTest extends ModuleUnitTestCase
      */
     public function testIsShowPrefillNotif($value, $expectedResult)
     {
-        $this->getConfig()->saveShopConfVar(null, 'blKlarnaPreFillNotification',  $value, $this->getShopId(), 'module:tcklarna');
+        $this->getConfig()->saveShopConfVar('bool', 'blKlarnaPreFillNotification',  $value, $this->getShopId(), 'module:tcklarna');
 
         $oViewConfig = oxNew(ViewConfig::class);
         $this->assertEquals($expectedResult, $oViewConfig->isShowPrefillNotif());
@@ -217,7 +217,7 @@ class KlarnaViewConfigTest extends ModuleUnitTestCase
      */
     public function testGetMode($value)
     {
-        $this->getConfig()->saveShopConfVar(null, 'sKlarnaActiveMode', $value, $this->getShopId(), 'module:tcklarna');
+        $this->getConfig()->saveShopConfVar('str', 'sKlarnaActiveMode', $value, $this->getShopId(), 'module:tcklarna');
         $oViewConfig = oxNew(ViewConfig::class);
         $this->assertEquals($value, $oViewConfig->getMode());
     }
@@ -305,8 +305,8 @@ class KlarnaViewConfigTest extends ModuleUnitTestCase
      */
     public function testGetKlarnaFooterContent($mode, $klFooterType, $klFooterValue, $expectedResult)
     {
-        $this->getConfig()->saveShopConfVar(null, 'sKlarnaFooterDisplay', $klFooterType, $this->getShopId(), 'module:tcklarna');
-        $this->getConfig()->saveShopConfVar(null, 'sKlarnaFooterValue', $klFooterValue, $this->getShopId(), 'module:tcklarna');
+        $this->getConfig()->saveShopConfVar('str', 'sKlarnaFooterDisplay', $klFooterType, $this->getShopId(), 'module:tcklarna');
+        $this->getConfig()->saveShopConfVar('str', 'sKlarnaFooterValue', $klFooterValue, $this->getShopId(), 'module:tcklarna');
         $this->setModuleMode($mode);
 
         $oViewConfig = oxNew(ViewConfig::class);
