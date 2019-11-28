b75bec3f748b95785ee545fa4d7603a8032bd2eb
Hugo Ferreira <ferreira@topconcepts.com>
2019-11-28 15:30:44 +0100

    Add adapter unit test files

---
+++ Tests/Unit/Core/Adapters/BasketCostAdapterTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace TopConcepts\Klarna\Tests\Unit\Core\Adapters;
+
+use TopConcepts\Klarna\Tests\Unit\ModuleUnitTestCase;
+
+class BasketCostAdapterTest extends ModuleUnitTestCase
+{
+
+}
---
+++ Tests/Unit/Core/Adapters/PaymentAdapter.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace TopConcepts\Klarna\Tests\Unit\Core\Adapters;
+
+use TopConcepts\Klarna\Tests\Unit\ModuleUnitTestCase;
+
+class PaymentAdapter extends ModuleUnitTestCase
+{
+
+}
