4e14676939e0fb8ab23917ba0ce68730d2509643
nils.baczynski@fatchip.de <IbJbFc21-8>
2023-03-23 17:38:14 +0100

    0095265 - add migration process

--- Core/KlarnaInstaller.php
+++ Core/KlarnaInstaller.php
@@ -27,7 +27,8 @@ use OxidEsales\Eshop\Core\Model\BaseModel;
 use OxidEsales\Eshop\Core\Registry;
 use OxidEsales\Eshop\Core\Field;
 use OxidEsales\Eshop\Core\Database\Adapter\Doctrine\Database;
-use TopConcepts\Klarna\Model\KlarnaPayment;
+use OxidEsales\DoctrineMigrationWrapper\MigrationsBuilder;
+use Symfony\Component\Console\Output\BufferedOutput;
 
 class KlarnaInstaller extends ShopConfiguration
 {
@@ -80,12 +81,25 @@ class KlarnaInstaller extends ShopConfiguration
         $instance = self::getInstance();
 
         $instance->checkAndUpdate();
-
-        $instance->extendDbTables();
-
         $instance->addConfigVars();
 
-        $instance->addKlarnaPaymentsMethods();
+        $instance->executeModuleMigrations();
+    }
+
+    /**
+     * Execute necessary module migrations on activate event
+     */
+    private static function executeModuleMigrations(): void
+    {
+        $migrations = (new MigrationsBuilder())->build();
+
+        $output = new BufferedOutput();
+        $migrations->setOutput($output);
+        $neeedsUpdate = $migrations->execute('migrations:up-to-date', 'tcklarna');
+
+        if ($neeedsUpdate) {
+            $migrations->execute('migrations:migrate', 'tcklarna');
+        }
     }
 
     protected function checkAndUpdate() {
@@ -108,15 +122,6 @@ class KlarnaInstaller extends ShopConfiguration
     {
         $config = Registry::getConfig();
         $shopId = $config->getShopId();
-
-//        $currencies    = Registry::getConfig()->getCurrencyArray();
-//        $currenciesVar = '';
-//        foreach ($currencies as $currency) {
-//            $currenciesVar .= $currency->name . '=>' . $currency->id;
-//            if ($currency !== end($currencies)) {
-//                $currenciesVar .= "\n";
-//            }
-//        }
 
         $defaultConfVars = array(
             'bool'   => array(
@@ -147,7 +152,6 @@ class KlarnaInstaller extends ShopConfiguration
                 'iKlarnaActiveCheckbox'            => KlarnaConsts::EXTRA_CHECKBOX_NONE,
                 'iKlarnaValidation'                => KlarnaConsts::NO_VALIDATION,
                 'sKlarnaAnonymizedProductTitle'    => 'anonymized product',
-//                'tcklarna_sKlarnaDefaultEURCountry'         => 'DE',
                 'sKlarnaFooterDisplay'             => 0,
 
 
@@ -165,7 +169,6 @@ class KlarnaInstaller extends ShopConfiguration
                     date_of_birth_mandatory => 0
                     national_identification_number_mandatory => 0
                     phone_mandatory => 0'
-                //                'tcklarna_aKlarnaCurrencies' => $currenciesVar,
             ),
             'select' => array(),
         );
@@ -191,245 +194,6 @@ class KlarnaInstaller extends ShopConfiguration
                 );
             }
         }
-    }
-
-    /**
-     * Add Klarna payment options
-     * @throws \Exception
-     */
-    protected function addKlarnaPaymentsMethods()
-    {
-        $oPayment = oxNew(BaseModel::class);
-        $oPayment->init('oxpayments');
-
-        $oPayment->load('oxidinvoice');
-        $de_prefix = $oPayment->getFieldData('oxdesc') === 'Rechnung' ? 0 : 1;
-        $en_prefix = $de_prefix === 1 ? 0 : 1;
-
-        $newPayments = array(
-            KlarnaPaymentTypes::KLARNA_PAYMENT_CHECKOUT_ID  =>
-                array($de_prefix => 'Klarna Checkout', $en_prefix => 'Klarna Checkout'),
-            KlarnaPaymentTypes::KLARNA_PAYMENT_ID  =>
-                array($de_prefix => 'One Klarna', $en_prefix => 'One Klarna'),
-        );
-
-        $sort   = -350;
-        $aLangs = Registry::getLang()->getLanguageArray();
-
-        if ($aLangs) {
-            foreach ($newPayments as $oxid => $aTitle) {
-                /** @var Payment $oPayment */
-                $oPayment = oxNew(BaseModel::class);
-                $oPayment->init('oxpayments');
-
-                $oPayment->load($oxid);
-                if ($oPayment->isLoaded()) {
-                    $oPayment->oxpayments__oxactive = new Field(1, Field::T_RAW);
-                    $oPayment->save();
-
-                    continue;
-                }
-//                $oPayment->setEnableMultilang(false);
-                $oPayment->setId($oxid);
-                $oPayment->oxpayments__oxactive      = new Field(1, Field::T_RAW);
-                $oPayment->oxpayments__oxaddsum      = new Field(0, Field::T_RAW);
-                $oPayment->oxpayments__oxaddsumtype  = new Field('abs', Field::T_RAW);
-                $oPayment->oxpayments__oxaddsumrules = new Field('31', Field::T_RAW);
-                $oPayment->oxpayments__oxfromboni    = new Field('0', Field::T_RAW);
-                $oPayment->oxpayments__oxfromamount  = new Field('0', Field::T_RAW);
-                $oPayment->oxpayments__oxtoamount    = new Field('1000000', Field::T_RAW);
-                $oPayment->oxpayments__oxchecked     = new Field(0, Field::T_RAW);
-                $oPayment->oxpayments__oxsort        = new Field(strval($sort), Field::T_RAW);
-                $oPayment->oxpayments__oxtspaymentid = new Field('', Field::T_RAW);
-
-                // set multi language fields
-                foreach ($aLangs as $oLang) {
-                    $sTag                                     = Registry::getLang()->getLanguageTag($oLang->id);
-                    $oPayment->{'oxpayments__oxdesc' . $sTag} = new Field($aTitle[$oLang->id], Field::T_RAW);
-                }
-
-                $oPayment->save();
-                $sort += 1;
-            }
-        }
-
-        $updateOxPayments =
-            array(
-                "UPDATE `oxpayments` SET `TCKLARNA_PAYMENTOPTION`='card' WHERE `oxid`='oxidcreditcard';",
-                "UPDATE `oxpayments` SET `TCKLARNA_PAYMENTOPTION`='direct banking' WHERE `oxid`='oxiddebitnote';",
-            );
-        foreach ($updateOxPayments as $sQuery) {
-            $this->db->execute($sQuery);
-        }
-    }
-
-    /**
-     * Extend klarna tables
-     * @throws \OxidEsales\Eshop\Core\Exception\DatabaseErrorException
-     */
-    protected function extendDbTables()
-    {
-
-        $sql = "
-            CREATE TABLE IF NOT EXISTS `tcklarna_logs` (
-                  `OXID`          CHAR(32)
-                                  CHARACTER SET latin1 COLLATE latin1_general_ci
-                               NOT NULL DEFAULT '',
-                
-                  `OXSHOPID`      CHAR(32)
-                                  CHARACTER SET latin1 COLLATE latin1_general_ci
-                               NOT NULL DEFAULT '',
-                  `TCKLARNA_METHOD`      VARCHAR(128)
-                                  CHARACTER SET utf8
-                               NOT NULL DEFAULT '',
-                  `TCKLARNA_REQUESTRAW`  TEXT CHARACTER SET utf8
-                               NOT NULL,
-                  `TCKLARNA_RESPONSERAW` TEXT CHARACTER SET utf8
-                               NOT NULL,
-                  `TCKLARNA_DATE`        DATETIME
-                               NOT NULL DEFAULT '0000-00-00 00:00:00',
-                  PRIMARY KEY (`OXID`),
-                  KEY `TCKLARNA_DATE` (`TCKLARNA_DATE`)
-                )
-                  ENGINE = InnoDB
-                  DEFAULT CHARSET = utf8;
-                  
-
-            CREATE TABLE IF NOT EXISTS `tcklarna_ack` (
-                  `OXID`       VARCHAR(32)
-                               CHARACTER SET latin1 COLLATE latin1_general_ci
-                                        NOT NULL,
-                  `KLRECEIVED` DATETIME NOT NULL,
-                  PRIMARY KEY (`OXID`)
-                )
-                  ENGINE = InnoDB
-                  COMMENT ='List of all Klarna acknowledge requests'
-                  DEFAULT CHARSET = utf8;
-                  
-                
-            CREATE TABLE IF NOT EXISTS `tcklarna_anon_lookup` (
-                  `TCKLARNA_ARTNUM` VARCHAR(32) NOT NULL,
-                  `OXARTID`  VARCHAR(32) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
-                  PRIMARY KEY (`TCKLARNA_ARTNUM`)
-                )
-                  ENGINE = InnoDB
-                  COMMENT ='Mapping of annonymous article numbers to their oxids'
-                  DEFAULT CHARSET = utf8;
-                  
-            CREATE TABLE IF NOT EXISTS `tcklarna_instant_basket` (
-                `OXID` VARCHAR(32) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
-                `BASKET_INFO` MEDIUMBLOB,
-                `STATUS`  VARCHAR(32) NOT NULL DEFAULT 'OPENED',
-                `TYPE` VARCHAR(32) NOT NULL DEFAULT '',
-                `TIMESTAMP` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Timestamp',
-                PRIMARY KEY (`OXID`)
-            )
-            ENGINE = InnoDB
-            DEFAULT CHARSET = utf8;";
-
-        $this->db->execute($sql);
-
-        $this->addAlterTables();
-
-        $this->updateViews();
-    }
-
-    /**
-     * @throws \OxidEsales\Eshop\Core\Exception\DatabaseErrorException
-     */
-    protected function addAlterTables()
-    {
-        $aStructure = array(
-            'oxorder'         => array(
-                'TCKLARNA_MERCHANTID' => 'ADD COLUMN `TCKLARNA_MERCHANTID` VARCHAR(128)  DEFAULT \'\' NOT NULL',
-                'TCKLARNA_SERVERMODE' => 'ADD COLUMN `TCKLARNA_SERVERMODE` VARCHAR(16) NOT NULL DEFAULT \'\'',
-                'TCKLARNA_ORDERID'    => 'ADD COLUMN `TCKLARNA_ORDERID` VARCHAR(128)  DEFAULT \'\' NOT NULL',
-                'TCKLARNA_SYNC'       => 'ADD COLUMN `TCKLARNA_SYNC` TINYINT UNSIGNED NOT NULL DEFAULT \'1\'',
-            ),
-            'oxorderarticles' => array(
-                'TCKLARNA_TITLE'  => 'ADD COLUMN  `TCKLARNA_TITLE` VARCHAR(255) NOT NULL DEFAULT \'\'',
-                'TCKLARNA_ARTNUM' => 'ADD COLUMN  `TCKLARNA_ARTNUM` VARCHAR(255) NOT NULL DEFAULT \'\'',
-            ),
-            'oxpayments'      => array(
-                'TCKLARNA_PAYMENTTYPES'           => 'ADD COLUMN `TCKLARNA_PAYMENTTYPES` SET(\'payment\',\'checkout\') NULL DEFAULT \'\'',
-                'TCKLARNA_EXTERNALNAME'           => 'ADD COLUMN `TCKLARNA_EXTERNALNAME` VARCHAR(255) NULL DEFAULT \'\'',
-                'TCKLARNA_EXTERNALPAYMENT'        => 'ADD COLUMN `TCKLARNA_EXTERNALPAYMENT` TINYINT(1) UNSIGNED NOT NULL DEFAULT \'0\'',
-                'TCKLARNA_EXTERNALCHECKOUT'       => 'ADD COLUMN `TCKLARNA_EXTERNALCHECKOUT` TINYINT(1) UNSIGNED NOT NULL DEFAULT \'0\'',
-                'TCKLARNA_PAYMENTIMAGEURL'        => 'ADD COLUMN `TCKLARNA_PAYMENTIMAGEURL` VARCHAR(255) NULL DEFAULT \'\'',
-                'TCKLARNA_PAYMENTIMAGEURL_1'      => 'ADD COLUMN `TCKLARNA_PAYMENTIMAGEURL_1` VARCHAR(255) NULL DEFAULT \'\'',
-                'TCKLARNA_PAYMENTIMAGEURL_2'      => 'ADD COLUMN `TCKLARNA_PAYMENTIMAGEURL_2` VARCHAR(255) NULL DEFAULT \'\'',
-                'TCKLARNA_PAYMENTIMAGEURL_3'      => 'ADD COLUMN `TCKLARNA_PAYMENTIMAGEURL_3` VARCHAR(255) NULL DEFAULT \'\'',
-                'TCKLARNA_CHECKOUTIMAGEURL'       => 'ADD COLUMN `TCKLARNA_CHECKOUTIMAGEURL` VARCHAR(255) NULL DEFAULT \'\'',
-                'TCKLARNA_CHECKOUTIMAGEURL_1'     => 'ADD COLUMN `TCKLARNA_CHECKOUTIMAGEURL_1` VARCHAR(255) NULL DEFAULT \'\'',
-                'TCKLARNA_CHECKOUTIMAGEURL_2'     => 'ADD COLUMN `TCKLARNA_CHECKOUTIMAGEURL_2` VARCHAR(255) NULL DEFAULT \'\'',
-                'TCKLARNA_CHECKOUTIMAGEURL_3'     => 'ADD COLUMN `TCKLARNA_CHECKOUTIMAGEURL_3` VARCHAR(255) NULL DEFAULT \'\'',
-                'TCKLARNA_PAYMENTOPTION'          => 'ADD COLUMN `TCKLARNA_PAYMENTOPTION` SET(\'card\',\'direct banking\',\'other\') NOT NULL DEFAULT \'other\'',
-                'TCKLARNA_EMDPURCHASEHISTORYFULL' => 'ADD COLUMN `TCKLARNA_EMDPURCHASEHISTORYFULL` TINYINT(1) UNSIGNED NOT NULL DEFAULT \'0\'',
-            ),
-            'oxaddress'       => array(
-                'TCKLARNA_TEMPORARY' => 'ADD COLUMN `TCKLARNA_TEMPORARY` TINYINT(1) UNSIGNED NOT NULL DEFAULT \'0\'',
-            ),
-            'tcklarna_logs'   => array(
-                'TCKLARNA_ORDERID'    => 'ADD COLUMN `TCKLARNA_ORDERID` VARCHAR(128) CHARACTER SET utf8 DEFAULT \'\' NOT NULL AFTER `OXID`',
-                'TCKLARNA_MID'        => 'ADD COLUMN `TCKLARNA_MID` VARCHAR(50) CHARACTER SET utf8 NOT NULL AFTER `OXSHOPID`',
-                'TCKLARNA_STATUSCODE' => 'ADD COLUMN `TCKLARNA_STATUSCODE` VARCHAR(16) CHARACTER SET utf8 NOT NULL AFTER `TCKLARNA_MID`',
-                'TCKLARNA_URL'        => 'ADD COLUMN `TCKLARNA_URL` VARCHAR(256) CHARACTER SET utf8 AFTER `TCKLARNA_METHOD`',
-            ),
-            'tcklarna_ack'    => array(
-                'TCKLARNA_ORDERID' => 'ADD COLUMN `TCKLARNA_ORDERID` VARCHAR(128) CHARACTER SET utf8 DEFAULT \'\' NOT NULL AFTER `OXID`, ADD KEY `TCKLARNA_ORDERID` (`TCKLARNA_ORDERID`)',
-            ),
-        );
-
-        // ADD COLUMNS
-        foreach ($aStructure as $sTableName => $aColumns) {
-
-            $query = "ALTER TABLE `$sTableName` ";
-            $first = true;
-
-            foreach ($aColumns as $sColumnName => $queryPart) {
-                if (!$this->dbColumnExist($sTableName, $sColumnName)) {
-                    if (!$first) {
-                        $query .= ', ';
-                    }
-                    $query .= $queryPart;
-                    $first = false;
-                }
-            }
-
-            $this->db->execute($query);
-        }
-    }
-
-
-    /**
-     * Checks if specific column exists in the table
-     * @param $sTableName
-     * @param $sColumnName
-     * @throws \OxidEsales\Eshop\Core\Exception\DatabaseErrorException
-     * @return bool
-     */
-    protected function dbColumnExist($sTableName, $sColumnName)
-    {
-        $query = "SELECT * FROM information_schema.COLUMNS
-                  WHERE TABLE_SCHEMA = '" . $this->dbName . "' 
-                  AND TABLE_NAME = '$sTableName'
-                  AND COLUMN_NAME = '$sColumnName'
-                  ";
-
-        return count($this->db->select($query)->fetchAll()) > 0;
-    }
-
-
-    /**
-     * Performs full view update
-     */
-    protected function updateViews()
-    {
-        //preventing edit for anyone except malladmin
-        //if (Registry::getSession()->getVariable("malladmin")) {
-        $oMetaData = oxNew(DbMetaDataHandler::class);
-        $oMetaData->updateViews();
-        //}
     }
 
     public static function onDeactivate()
