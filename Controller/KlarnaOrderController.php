193bf9cfe5389c50ad774887a283bdda0101faae
nils.baczynski@fatchip.de <IbJbFc21-8>
2024-09-16 14:47:37 +0200

    0105790 - fix auth token being set incorrectly resulting in order creation error

--- Controller/KlarnaOrderController.php
+++ Controller/KlarnaOrderController.php
@@ -131,7 +131,7 @@ class KlarnaOrderController extends KlarnaOrderController_parent
                 $oSession->setVariable('kebmail', $address["email"]);
                 $oSession->setVariable('deladrid', $deladrid);
             }
-            $oSession->setVariable('sAuthToken', $kebauthresponse->client_token);
+            $oSession->setVariable('sAuthToken', $kebauthresponse->session_id);
             $oSession->setVariable('finalizeRequired', $kebauthresponse->finalize_required);
 
         }
